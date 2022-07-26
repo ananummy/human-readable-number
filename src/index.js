95fe8330e52ef19d95159e2af3876da2c4447c59
Anastasia E <90329657+ananummy@users.noreply.github.com>
2022-07-27 00:56:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function toReadable (number) {
-  
+    const a = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen, sixteen', 'sventeen', 'eighteen', 'nineteen']
+    const b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    const c = ' hundred'
+
+    if (number === 0) return 'zero'
+
+    let num = number.toString().split("")
+
+    if (num.length == 1) {
+        return (a[+num[0]]).trim()
+    } else if((num.length == 2) & ((num[0] + num[1]) < 20)) {
+        return (a[+(num[0] + num[1])]).trim()
+    } else if((num.length == 2) & ((num[0] + num[1]) >= 20)) {
+        return  (b[+num[0]] + ' ' + a[+num[1]]).trim();
+    } else if((num.length == 3) & ((num[1] + num[2]) < 20)) {
+        return (a[+num[0]] + c + a[+(num[1]+num[2])]).trim();
+    } else if((num.length == 3) & ((num[1] + num[2]) >= 20)) {
+        return (a[+num[0]] + c + b[+num[1]] + ' ' + a[+num[2]]).trim();
+    }
 }
